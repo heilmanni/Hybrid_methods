da3872a790a6755c1741307b941703d4d8a50701
heilmanni <56125449+heilmanni@users.noreply.github.com>
2020-02-04 00:27:39 +0100

    Update R_code.R

--- R_code.R
+++ R_code.R
@@ -1,4 +1,4 @@
-#This file forecast in three different way: ARIMA, ANN and hibrid methology will be used
+#This file forecast in three different ways: ARIMA, ANN and hibrid methology will be used
 
 #packages
 library(tseries)
@@ -7,69 +7,55 @@ library(ModelMetrics)
 library(neuralnet)
 library(xts)
 library(gets)
+library(zoo)
 
 #IMPORT TIMESERIES
-#you have to import the phil_data.R file
-#the output variable is the inflation; there is two given option, based on GDP-deflator and PCE deflator
+#phil_data.R file has to be imported
+#there are missing values in the last row
+phil_data <- phil_data[1:243,]
+
+#the output variable is the inflation; there is two options, based on GDP deflator and PCE deflator
 y_GDP <- phil_data[,2]
+y_GDP <- y_GDP[[1]]
 y_PCE <- phil_data[,3]
+y_PCE <- y_PCE[[1]]
 
 #the regressors are the columns 3 to 9
-x_reg <- phil_data[,3:9]
+x_reg <- phil_data[,4:9]
+x_reg <- cbind(x_reg[[1]], x_reg[[2]], x_reg[[3]], x_reg[[4]], x_reg[[5]], x_reg[[6]])
 
+#1. ARX MODEL
+glm(y_GDP ~ xreg)
+Box.test(x = resid(glm(y_GDP ~ xreg)), type = "Ljung")
+#autocorrelation is presented
 
-#making training, test and out-of-sample sets
-training <- RawData[1:950]
-test <- RawData[951:1000]
-in_sample <- RawData[1:1000]
-out_sample <- RawData[1001:1010]
+#you should source arx_forecast function
+arx_forecast(lisd = 230, losd = 13, y = y_GDP, xreg = x_reg, max_ar = 5)
+arx_forecast(lisd = 230, losd = 13, y = y_PCE, xreg = x_reg, max_ar = 5)
 
+#the best model is that where the MSE is the smallest and the p-value of Ljung-Box test are larger than 0.05
+#in both cases this is when phi = 1
 
-#1. ARIMA MODEL
-#ADF test, number of differencies
-adf.test(in_sample)
-ndiffs(in_sample)
+#MSE = 0.1295500
 
-#making a stationary timeseries from the original one
-stationary <- data.frame()
-stationary <- diff(in_sample, differences = 1)
-stationary[1] <- 0
-
-stationary_out <- diff(out_sample, differences = 1)
-stationary_out[1] <- 0
-
-#let's have a look at the correlograms
-acf(stationary)
-pacf(stationary)
-
-#you should source the arima_prediction function
-#I will use an ARX modell, so the MA member should be 0
-model_arima <- arima_prediction(training = in_sample, test = out_sample, ar = 7, ma = 0, d = 1)
-
-#select the rowes where p-value are larger than 0.05
-model_arima <- model_arima[which(model_arima[,4] > 0.05), ]
-
-#the best model is where MSE is the smallest
-model_arima[which.min(model_arima[,3]),]
-
-#to have an ARX model you can use arx function ('gets' package)
-arx <- arx
+forecast_arx <- arx(y = y_in_sample, ar = phi, mxreg = x_reg_in_sample, mc = T)
 
 
 #NEURAL NETWORKS METHODOLOGY
-#you should source neural_netwrok function
-model_ann <- neural_network(training = training, test = test, dimension = 3, nodes = 3)
+#ann_forecast function has to be sourced
+model_ann <- ann_forecast(ltrain = 200, ltest = 30, y = y_PCE, xreg = x_reg, max_dim = 4, max_nodes = 4)
+
+#forecasting on the out-of-sample horizon with the best combination of dimensions and nodes
 model_ann[which.min(model_ann[,3]),]
+#now dim = 3, nodes = 4
 
-proba <- model_ann[which.min(model_ann[,3]),]
+#for comparing a forecast and error value must be given for the out-of-sample data
+ann_out_forecast(lisd = 230, losd = 13, dim = 1, nodes = 4, y = y_GDP, xreg = x_reg)
 
-#forecasting for out-of-sample set
-
+#MSE = 0.143295
 
 #HYBRID METHODOLOGY
-#ARIMA model on the original timeseries
-#ANN model on the residuals of original time series
-fcast_hybrid <- hybrid_forecast(in_sample = in_sample, h = 10, ar = 2, ma = 0, d = 1, p = 2, size = 3, brake = 0.95)
+#required functions: arx_forecast, ann_forecast
+hybrid_forecast(y = y_GDP, xreg = x_reg, lisd = 230, losd = 13, phi = 1, brake = 0.85, max_dim = 2, max_nodes = 2)
 
-
-
+MSE_hybrid <- 0.1149234
