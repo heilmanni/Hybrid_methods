6e31e7cc4cd4d7d258dd40f2ccd611a5c694a3de
heilmanni <56125449+heilmanni@users.noreply.github.com>
2020-02-04 20:50:51 +0100

    Add files via upload

---
+++ sum_res.R
@@ -0,0 +1,21 @@
+#this function give a matrix as result which contains the cumulated errors elements by elements
+#res_data should be a matrix which contains residuals of time-series
+
+
+sum_res <- function(res_data)
+{
+  ntm <- nrow(res_data) #number of rows is the number of the tested methods
+  losd <- ncol(res_data) #number of columns is the length of out-of-sample data
+  
+  sum_res <- data.frame() #creating a dataframe
+  
+  for (j in 1:ntm)
+  {
+    for (i in 1:losd)
+    {
+      sum_res[j,i] <- sum(abs(res_GDP[j,1:i])) #cumulating the absolute value of the residuals in each row
+    }
+  }
+  colnames(sum_res) <- c(1:losd)
+  return(sum_res)
+}
