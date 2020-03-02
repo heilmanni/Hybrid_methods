ea870bc2adf8cff0cf9ce145171ec0dffa780f26
heilmanni <56125449+heilmanni@users.noreply.github.com>
2020-03-02 16:47:52 +0100

    Update hybrid_forecast.R

--- hybrid_forecast.R
+++ hybrid_forecast.R
@@ -1,20 +1,21 @@
 #this function calculates an MSE value based on a hybrid method and shows the predicted values
-#y is the result timeseries - it must be a ts or vector
-#xreg is vector or matrix which contains the regressors
-#lisd is the length of in_sample timeseries
-#losd is the length of out_of_sample timeseries
+#y is the result time-series - it must be a ts or vector
+#xreg is a vector or a matrix which contains the regressors
+#lisd is the length of in_sample time-series
+#losd is the length of out_of_sample time-series
 #lisd and losd should be positive integers; lisd + losd cannot be longer than y
 #phi is the optimal number of ar lag - the best arx model can be calculated with arx_forecast function, please use the same parameters as in the first part of analysis
 #brake should be given between 0 and 1 - it refers to the proportion of the training set to the whole in the ann model, based on the residuals of arx model
-#max_dim shows the maximum number of embedding dimensions for testing - it must be a positive integer; 0 to max_dim the results will be  calculated
-#max_nodes shows the maximum number of nodes for testing - it must be a positive integer; 0 to max_nodes the results will be calculated
-#required functions: arx_forecast, ann_forecast
+#max_dim shows the maximum number of embedding dimensions for testing - it must be a positive integer; from 0 to max_dim the results will be  calculated
+#max_nodes shows the maximum number of nodes for testing - it must be a positive integer; from 0 to max_nodes the results will be calculated
+#further required functions: arx_forecast, ann_forecast
 
 hybrid_forecast <- function(y, xreg, lisd, losd, phi, brake, max_dim, max_nodes)
 {
-  #creating a vector for the forecasts
+  #creating a vector for the forecast
   predict <- c()
   
+  #making a rolling window with the same length as the out_of_sample
   for (pred in 0:(losd-1))
   {
     y_in_sample <- y[(1+pred):(lisd+pred)] #y has always the same length, but the start and ending points are different
@@ -43,15 +44,15 @@ hybrid_forecast <- function(y, xreg, lisd, losd, phi, brake, max_dim, max_nodes)
     nn_fcast <- as.numeric(forecast(object = fitted(nnetar(y = arx_res, xreg = x_reg_in_sample[(phi+1):lisd,],
                                                            p = best_p, size = best_size, repeats = 10)),
                                     h = 1)[[2]]) #nn forecast for the next period's residual
-    fcast <- arx_fcast + nn_fcast #the forecast is the sum of two predictions
+    fcast <- arx_fcast + nn_fcast #the forecast is the sum of the two predictions
     
     predict <- c(predict, fcast) #binding the earlier and new forecasts
     
     print(pred)
   }
   
-  MSE_hybrid <- mse(actual = y[(lisd+1):(lisd+losd)], predicted = predict)
-  final <- c(MSE_hybrid, predict)
+  MSE_hybrid <- mse(actual = y[(lisd+1):(lisd+losd)], predicted = predict) 
+  final <- c(MSE_hybrid, predict) #final results
   
   return(final)
 }
