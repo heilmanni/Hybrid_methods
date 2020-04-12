1314a54be47fc0ea385ccc3a2dd7b90fd872d679
heilmanni <56125449+heilmanni@users.noreply.github.com>
2020-04-13 00:55:02 +0200

    Changing the structure of the ANN-model

--- hybrid_forecast.R
+++ hybrid_forecast.R
@@ -9,53 +9,59 @@
 #max_dim shows the maximum number of embedding dimensions for testing - it must be a positive integer; from 0 to max_dim the results will be  calculated
 #max_nodes shows the maximum number of nodes for testing - it must be a positive integer; from 0 to max_nodes the results will be calculated
 
+
 #further required functions: arx_forecast, ann_forecast
 source(file = "https://raw.githubusercontent.com/heilmanni/Hybrid_methods/master/arx_forecast.R")
-source(file = "https://raw.githubusercontent.com/heilmanni/Hybrid_methods/master/ann_forecast.R")
 
 hybrid_forecast <- function(y, xreg, lisd, losd, phi, brake, max_dim, max_nodes)
 {
-  #creating a vector for the forecast
-  predict <- c()
-  
-  #making a rolling window with the same length as the out_of_sample
-  for (pred in 0:(losd-1))
+
+  final <- data.frame() #creating a data frame for the final results
+  losd_number <- c(1:losd) #a vector from 1 up to the length of the out-of-sample
+    
+  for (dim in 1:max_dim)
   {
-    y_in_sample <- y[(1+pred):(lisd+pred)] #y has always the same length, but the start and ending points are different
-    x_reg_in_sample <- xreg[(1+pred):(lisd+pred),] #x has always the same length, but the start and ending points are different
+    print(dim)
     
-    
-    #calculating the best parameters for the forecasting
-    arx_model <- gets::arx(y = y_in_sample, ar = phi, mxreg = x_reg_in_sample, mc = T) #an arx model with a given ar lag
-    arx_res <- arx_model$residuals #residuals of the model
-    
-    #creating a test and training set at a brakepoint (which gives the proportion of the training set to the whole)
-    border <- as.integer(length(arx_res)*brake)
-    
-    #searching for the best ann modell on the residuals of arx_model
-    #required function: ann_forecast
-    arx_res_ann <- ann_forecast(ltrain = border, ltest = (length(arx_res)-border), y = arx_res, 
-                                xreg = x_reg_in_sample, max_dim = max_dim, max_nodes = max_nodes)
-    
-    #choosing the best model based on mse
-    best_param <- arx_res_ann[which.min(arx_res_ann[,3]),]
-    best_p <- as.numeric(best_param[1])
-    best_size <- as.numeric(best_param[2])
-    
-    #forecasting with the given params
-    arx_fcast <- as.numeric(forecast::forecast(object = fitted(arx_model), h = 1)[[2]]) #arx forecast for the next period
-    nn_fcast <- as.numeric(forecast::forecast(object = fitted(forecast::nnetar(y = arx_res, xreg = x_reg_in_sample[(phi+1):lisd,],
-                                                           p = best_p, size = best_size, repeats = 10)),
-                                    h = 1)[[2]]) #nn forecast for the next period's residual
-    fcast <- arx_fcast + nn_fcast #the forecast is the sum of the two predictions
-    
-    predict <- c(predict, fcast) #binding the earlier and new forecasts
-    
-    print(pred)
+    for (nodes in 1:max_nodes)
+    {
+      
+      #creating a vector for the forecast
+      predict <- c()
+      
+      #making a rolling window with the same length as the out_of_sample
+      for (pred in 0:(losd-1))
+      {
+        y_in_sample <- y[(1+pred):(lisd+pred)] #y has always the same length, but the start and ending points are different
+        x_reg_in_sample <- xreg[(1+pred):(lisd+pred),] #x has always the same length, but the start and ending points are different
+        
+        
+        #calculating the best parameters for the forecasting
+        arx_model <- gets::arx(y = y_in_sample, ar = phi, mxreg = x_reg_in_sample, mc = T) #an arx model with a given ar lag
+        arx_res <- arx_model$residuals #residuals of the model
+        
+        #creating a test and training set at a brakepoint (which gives the proportion of the training set to the whole)
+        border <- as.integer(length(arx_res)*brake)
+        
+        #forecasting with the given params
+        arx_fcast <- as.numeric(forecast::forecast(object = fitted(arx_model), h = 1)[[2]]) #arx forecast for the next period
+        nn_fcast <- as.numeric(forecast::forecast(object = fitted(forecast::nnetar(y = arx_res, xreg = x_reg_in_sample[(phi+1):lisd,],
+                                                                                   p = dim, size = nodes, repeats = 10)),
+                                                  h = 1)[[2]]) #nn forecast for the residual of the next period
+        fcast <- arx_fcast + nn_fcast #the forecast is the sum of the two predictions
+        
+        predict <- c(predict, fcast) #binding the earlier and new forecasts
+        
+      }
+      
+      MSE_hybrid <- ModelMetrics::mse(actual = y[(lisd+1):(lisd+losd)], predicted = predict) #MSE-value of the predicted values
+      final <- rbind(final, c(dim, nodes, MSE_hybrid, predict)) #binding the most important information
+      colnames(final) <- c("dim", "nodes", "MSE_hybrid", losd_number)
+    }
   }
   
-  MSE_hybrid <- ModelMetrics::mse(actual = y[(lisd+1):(lisd+losd)], predicted = predict) 
-  final <- c(MSE_hybrid, predict) #final results
+  #choosing the best model based on mse
+  best_model <- final[which.min(final[,3]),]
   
-  return(final)
+  return(best_model)
 }
